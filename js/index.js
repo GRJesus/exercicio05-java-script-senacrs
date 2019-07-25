0c5839ec847e969edcf1fa6de193f47043019e7a
GRJesus <rod.giu@hotmail.com>
2019-07-25 20:40:23 -0300

    teste

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//Solicitar ao usuario 3 dados
+//Numero 1
+//numero 2
+//operação */-+
+//usar if, else if parseInt document.body.append
+//mostrar o resultado
+
+var valor1 = (prompt("Digite o primeiro valor"));
+var operation = (prompt("Insira a operação"));
+var valor2 = (prompt("Digite o segundo valor"));
