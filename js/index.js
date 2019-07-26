9c59dea5b0ca351ccee30680d0b8a822b4898d06
GRJesus <rod.giu@hotmail.com>
2019-07-25 21:29:08 -0300

    Calculadora

--- js/index.js
+++ js/index.js
@@ -5,6 +5,26 @@
 //usar if, else if parseInt document.body.append
 //mostrar o resultado
 
-var valor1 = (prompt("Digite o primeiro valor"));
-var operation = (prompt("Insira a operação"));
-var valor2 = (prompt("Digite o segundo valor"));
+var valor1 = parseInt(prompt("Digite o primeiro valor"));
+var operation = prompt("Insira a operação * ou / ou + ou -");
+var valor2 = parseInt(prompt("Digite o segundo valor"));
+var result = ""
+var errormessage = "Favor verifique os valores digitados"
+
+if (operation === "/") {
+  result = valor1 / valor2;
+} else if (operation === "*") {
+  result = valor1 * valor2;
+} else if (operation === "+") {
+  result = valor1 + valor2;
+} else if (operation === "-") {
+  result = valor1 - valor2;
+} else {
+  result = errormessage;
+}
+if (result != operation){
+  result = errormessage
+} else if (result = " "){
+  result = errormessage
+}
+document.body.append (result);
